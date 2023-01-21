15dae2c1293cc354fb0a3dee9014f3cb094e3bbe
wombatstar <toxaf13@gmail.com>
2023-01-21 15:38:49 +0100

    1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,90 +1,17 @@
-const express = require('express')
-const joi = require("joi");
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const ctrlTask = require('../../controller/index');
 
-const {
-   listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-  createNewContact,
-  getUpdatedContact,
- } = require("./../../models/contacts");
+router.get('/', ctrlTask.get);
 
-/////VALIDATE/////
- 
-const contactSchema = joi.object({
-  name: joi.string().min(3).required(),
-  email: joi.string().email().required(),
-  phone: joi.string().min(9).required(),
-});
+router.get('/:id', ctrlTask.getById);
 
-/////GET/////
+router.post('/', ctrlTask.addContact);
 
-router.get('/', async (req, res, next) => {
-   const contacts = await listContacts();
-   res.status(200).json(contacts);
-})
+router.put('/:id', ctrlTask.updateContact);
 
-/////GETBYID/////
+//router.patch('/:id/status', ctrlTask.updateStatus);
 
-router.get('/:contactId', async (req, res, next) => {
-   const { contactId } = req.params;
-   const contact = await getContactById(contactId);
-   if(!contact) return res.status(404).json({message:"Contact not found"});
-   if(contact) res.status(200).json(contact);
-})
+router.delete('/:id', ctrlTask.deleteContact);
 
-/////POST/////
-
-router.post('/', async (req, res, next) => {
-   const { name, email, phone} = req.body;
-   const {error, value} = contactSchema.validate(req.body);
-   if(error){
-      console.log(error);
-      return res.send('Invalid Request')
-   }
-   if (!name || !email || !phone)
-       return res.status(400).json({message:"missing required name field"});
-       const newContact = await createNewContact(req.body)
-      await addContact(req.body);
-      res.status(201).send(newContact)
-})
-
-/////DELETE/////
-
-router.delete('/:contactId', async (req, res, next) => {
-  const {contactId} = req.params;
-  const contact = await getContactById(contactId);
-  if(!contact ) return res.status(404).json({message:'Contact not found'});
-  else {await removeContact(contactId);
-res.status(200).json({message:'Contact deleted'})}
-})
-
-/////PUT/////
-
-router.put('/:contactId', async (req, res, next) => {
-  const {name, email, phone} = req.body;
-  const {contactId} = req.params;
-
-  const {error, value} = contactSchema.validate(req.body);
-  if(error){
-     console.log(error);
-     return res.send({message: error.details[0].message})}
-
-  if (!name && !email && !phone)
-   return res.status(400).json({message:"missing fields"});
-   const contact = await getContactById(contactId);
-
-   if(!contact) return res.status(404).json({ message: 'Not Found'});
-   if (contact){
-      const updated = await getUpdatedContact(contact, req.body);
-      await updateContact(contactId, req.body);
-      res.status(200).send(updated);
-      
-      
-   }
-})
-
-module.exports = router
+module.exports = router;
---
+++ service/schemas/contact.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose');
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: 3,
+      maxlength: 11,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      minlength: 10,
+      maxlength: 20,
+    },
+    phone: {
+      type: String,
+      },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+//  { versionKey: false, timestamps: true },
+);
+
+const Contact = model('contact', contactSchema);
+
+module.exports = Contact;
